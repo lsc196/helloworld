40d2a2ef024f05164ce04f0a4de2ef4a109951f8
lsc196 <1084875445@qq.com>
2020-07-02 18:23:57 +0800

    增加了首页接口

--- src/main/java/com/example/helloworld/controller/Controller.java
+++ src/main/java/com/example/helloworld/controller/Controller.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/")
     public String index(){
         return "Hello World!";
     }
